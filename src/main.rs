ecb03e561180c626eab4918a47b25a45201ab8cb
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2019-12-09 09:38:22 +0100

    Change key_value size storage format

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,28 @@ use std::io::{Read, Seek, Write};
 use std::path::Path;
 
 fn append(file: &mut File, key: &str, value: &str) {
-    let key_len = key.len() as u8;
-    let value_len = value.len() as u16;
-    file.write_all(&key_len.to_le_bytes());
+    let key_len = (key.len() as u8).to_be_bytes();
+    let value_len = (value.len() as u16).to_be_bytes();
+    let mut len_buf = [key_len[0], value_len[0], value_len[1]];
+    file.write_all(&len_buf);
     file.write_all(key.as_bytes());
-    file.write_all(&value_len.to_le_bytes());
     file.write_all(value.as_bytes());
 }
 
-fn read_key(file: &mut File) -> String {
-    let mut buf = [0u8; 1];
-    String::from_utf8(read(file, &mut buf)).unwrap()
-}
+fn read_key_value(file: &mut File) -> String {
+    let mut len_buf = [0u8; 3];
+    file.read_exact(&mut len_buf);
 
-fn read_value(file: &mut File) -> String {
-    let mut buf = [0u8; 2];
-    String::from_utf8(read(file, &mut buf)).unwrap()
-}
+    let key_len = len_buf[0] as usize;
+    let value_len = u16::from_be_bytes([len_buf[1], len_buf[2]]) as usize;
+    let len = key_len + value_len;
 
-fn read(file: &mut File, len_buf: &mut [u8]) -> Vec<u8> {
-    file.read_exact(len_buf);
-    let len = len_buf[0];
+    println!("{} + {} = {}", key_len, value_len, len);
+
     let mut buf = vec![0u8; len as usize];
     file.read_exact(&mut buf);
-    return buf;
+
+    String::from_utf8(buf).unwrap()
 }
 
 fn main() {
@@ -38,7 +36,7 @@ fn main() {
         .open(path)
         .unwrap();
 
-    println!("{} => {}", read_key(&mut file), read_value(&mut file));
+    println!("{}", read_key_value(&mut file));
 
     append(&mut file, "test_key", "test_value");
 }
