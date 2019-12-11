a7e21e1214df7c0830818c6efff2bf2cc02265e2
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2019-12-11 09:29:08 +0100

    Return key,value tuple from read_key_value

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 use std::fs::{File, OpenOptions};
-use std::io::{Read, Seek, Write};
+use std::io::{Read, Result, Write};
 use std::path::Path;
 
-fn append(file: &mut File, key: &str, value: &str) {
-    let key_len = key.len();
-    let key_bytes = (key_len as u8).to_be_bytes();
-    let value_len = value.len();
-    let value_bytes = (value_len as u16).to_be_bytes();
+fn append(file: &mut File, key: &str, value: &str) -> Result<()> {
+    let key_bytes: [u8; 1] = (key.len() as u8).to_be_bytes();
+    let value_bytes: [u8; 2] = (value.len() as u16).to_be_bytes();
 
-    let mut buf = vec![key_bytes[0], value_bytes[0], value_bytes[1]];
+    let mut buf: Vec<u8> = vec![key_bytes[0], value_bytes[0], value_bytes[1]];
     buf.append(&mut [key.as_bytes(), value.as_bytes()].concat());
 
-    file.write_all(&buf);
+    file.write_all(&buf)
 }
 
-fn read_key_value(file: &mut File) -> String {
+fn read_key_value(file: &mut File) -> Result<(String, String)> {
     let mut len_buf = [0u8; 3];
-    file.read_exact(&mut len_buf);
+    file.read_exact(&mut len_buf)?;
 
     let key_len = len_buf[0] as usize;
     let value_len = u16::from_be_bytes([len_buf[1], len_buf[2]]) as usize;
@@ -25,9 +23,12 @@ fn read_key_value(file: &mut File) -> String {
     println!("{} + {} = {}", key_len, value_len, len);
 
     let mut buf = vec![0u8; len as usize];
-    file.read_exact(&mut buf);
+    file.read_exact(&mut buf)?;
 
-    String::from_utf8(buf).unwrap()
+    Ok((
+        String::from_utf8_lossy(&buf[0..key_len]).into_owned(),
+        String::from_utf8_lossy(&buf[key_len..(len)]).into_owned(),
+    ))
 }
 
 fn main() {
@@ -39,7 +40,10 @@ fn main() {
         .open(path)
         .unwrap();
 
-    println!("{}", read_key_value(&mut file));
+    match read_key_value(&mut file) {
+        Err(why) => println!("{}", why),
+        Ok((key, value)) => println!("{} => {}", key, value),
+    }
 
     append(&mut file, "test_key", "test_value");
 }
