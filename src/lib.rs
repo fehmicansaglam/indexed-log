94f0d3318ea46d75f09c2ccf3b86e2d26d391c5f
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2019-12-13 10:13:15 +0100

    Add in memory hash table

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,78 @@
+use std::collections::HashMap;
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::{Read, Result, Seek, SeekFrom, Write};
+use std::path::Path;
 
-pub fn append(file: &mut File, key: &str, value: &str) -> Result<()> {
+pub struct IndexedLog<'a> {
+    index: HashMap<&'a str, u64>,
+    file: File,
+}
+
+impl<'a> IndexedLog<'a> {
+    pub fn new() -> Self {
+        let path = Path::new("./.indexed-log.db");
+        let file = OpenOptions::new()
+            .create(true)
+            .read(true)
+            .append(true)
+            .open(path)
+            .unwrap();
+
+        IndexedLog {
+            index: HashMap::new(),
+            file,
+        }
+    }
+
+    pub fn append(&mut self, key: &'a str, value: &'a str) {
+        let position = append_to_file(&mut self.file, key, value).unwrap();
+        self.index.insert(key, position);
+    }
+
+    pub fn print_all(&self) {
+        for (key, &offset) in self.index.iter() {
+            println!("{}: {}", key, offset);
+        }
+    }
+
+    pub fn read_all(&mut self) {
+        self.file.seek(SeekFrom::Start(0));
+
+        println!("Current db file state");
+        println!("---------------------");
+        loop {
+            match read_key_value(&mut self.file) {
+                Err(why) => {
+                    println!("Reached EOF: {}", why);
+                    break;
+                }
+                Ok((key, value)) => println!("{} => {}", key, value),
+            }
+        }
+        println!("---------------------");
+    }
+}
+
+fn append_to_file(file: &mut File, key: &str, value: &str) -> Result<u64> {
     let key_bytes: [u8; 1] = (key.len() as u8).to_be_bytes();
     let value_bytes: [u8; 2] = (value.len() as u16).to_be_bytes();
 
     let mut buf: Vec<u8> = vec![key_bytes[0], value_bytes[0], value_bytes[1]];
     buf.append(&mut [key.as_bytes(), value.as_bytes()].concat());
 
-    file.write_all(&buf)
+    let position = file.seek(SeekFrom::Current(0));
+    file.write_all(&buf);
+    position
 }
 
-pub fn read_key_value(file: &mut File) -> Result<(String, String)> {
+fn read_key_value(file: &mut File) -> Result<(String, String)> {
     let mut len_buf = [0u8; 3];
     file.read_exact(&mut len_buf)?;
 
     let key_len = len_buf[0] as usize;
     let value_len = u16::from_be_bytes([len_buf[1], len_buf[2]]) as usize;
     let len = key_len + value_len;
-
-    println!("{} + {} = {}", key_len, value_len, len);
 
     let mut buf = vec![0u8; len as usize];
     file.read_exact(&mut buf)?;
@@ -28,18 +81,4 @@ pub fn read_key_value(file: &mut File) -> Result<(String, String)> {
         String::from_utf8_lossy(&buf[0..key_len]).into_owned(),
         String::from_utf8_lossy(&buf[key_len..(len)]).into_owned(),
     ))
-}
-
-pub fn read_all(file: &mut File) {
-    file.seek(SeekFrom::Start(0));
-
-    loop {
-        match read_key_value(file) {
-            Err(why) => {
-                println!("Reached EOF: {}", why);
-                break;
-            }
-            Ok((key, value)) => println!("{} => {}", key, value),
-        }
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,31 @@ extern crate rand;
 
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
-use std::fs::OpenOptions;
-use std::io::Result;
-use std::path::Path;
+
+use crate::lib::IndexedLog;
 
 mod lib;
 
-fn main() -> Result<()> {
-    let path = Path::new("./.indexed-log.db");
-    let mut file = OpenOptions::new()
-        .create(true)
-        .read(true)
-        .append(true)
-        .open(path)?;
+fn main() {
+    let mut indexed_log = IndexedLog::new();
 
-    lib::read_all(&mut file);
+    indexed_log.read_all();
 
+    let (rand_key, rand_value) = random_key_value();
+    indexed_log.append(&rand_key, &rand_value);
+
+    let (rand_key, rand_value) = random_key_value();
+    indexed_log.append(&rand_key, &rand_value);
+
+    let (rand_key, rand_value) = random_key_value();
+    indexed_log.append(&rand_key, &rand_value);
+
+    indexed_log.print_all();
+    indexed_log.read_all();
+}
+
+fn random_key_value() -> (String, String) {
     let rand_key: String = thread_rng().sample_iter(&Alphanumeric).take(11).collect();
-
     let rand_value: String = thread_rng().sample_iter(&Alphanumeric).take(19).collect();
-
-    lib::append(&mut file, &rand_key, &rand_value)
+    (rand_key, rand_value)
 }
