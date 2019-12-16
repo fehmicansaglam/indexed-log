aa6fac8efa012ca277eefea752986528548de46e
Fehmi Can Saglam <fehmican.saglam@gmail.com>
2019-12-16 09:21:45 +0100

    Implement read from file by position

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,15 @@ use std::fs::OpenOptions;
 use std::io::{Read, Result, Seek, SeekFrom, Write};
 use std::path::Path;
 
+#[derive(Debug, Copy, Clone)]
+struct Position {
+    offset: u64,
+    key_len: u8,
+    value_len: u16,
+}
+
 pub struct IndexedLog<'a> {
-    index: HashMap<&'a str, u64>,
+    index: HashMap<&'a str, Position>,
     file: File,
 }
 
@@ -30,9 +37,14 @@ impl<'a> IndexedLog<'a> {
         self.index.insert(key, position);
     }
 
+    pub fn read(&mut self, key: &'a str) -> String {
+        let position = self.index.get(&key).unwrap();
+        read_from_file(&mut self.file, &position).unwrap().1
+    }
+
     pub fn print_all(&self) {
-        for (key, &offset) in self.index.iter() {
-            println!("{}: {}", key, offset);
+        for (key, &position) in self.index.iter() {
+            println!("{}: {:?}", key, position);
         }
     }
 
@@ -42,7 +54,7 @@ impl<'a> IndexedLog<'a> {
         println!("Current db file state");
         println!("---------------------");
         loop {
-            match read_key_value(&mut self.file) {
+            match read_next_from_file(&mut self.file) {
                 Err(why) => {
                     println!("Reached EOF: {}", why);
                     break;
@@ -54,24 +66,45 @@ impl<'a> IndexedLog<'a> {
     }
 }
 
-fn append_to_file(file: &mut File, key: &str, value: &str) -> Result<u64> {
-    let key_bytes: [u8; 1] = (key.len() as u8).to_be_bytes();
-    let value_bytes: [u8; 2] = (value.len() as u16).to_be_bytes();
+fn append_to_file(file: &mut File, key: &str, value: &str) -> Result<Position> {
+    let key_len = key.len() as u8;
+    let value_len = value.len() as u16;
+    let key_bytes: [u8; 1] = key_len.to_be_bytes();
+    let value_bytes: [u8; 2] = value_len.to_be_bytes();
 
     let mut buf: Vec<u8> = vec![key_bytes[0], value_bytes[0], value_bytes[1]];
     buf.append(&mut [key.as_bytes(), value.as_bytes()].concat());
 
-    let position = file.seek(SeekFrom::Current(0));
-    file.write_all(&buf);
-    position
+    let offset = file.seek(SeekFrom::Current(0))?;
+    file.write_all(&buf).map(|_| Position {
+        offset,
+        key_len,
+        value_len,
+    })
 }
 
-fn read_key_value(file: &mut File) -> Result<(String, String)> {
+fn read_next_from_file(file: &mut File) -> Result<(String, String)> {
     let mut len_buf = [0u8; 3];
     file.read_exact(&mut len_buf)?;
 
     let key_len = len_buf[0] as usize;
     let value_len = u16::from_be_bytes([len_buf[1], len_buf[2]]) as usize;
+    let len = key_len + value_len;
+
+    let mut buf = vec![0u8; len as usize];
+    file.read_exact(&mut buf)?;
+
+    Ok((
+        String::from_utf8_lossy(&buf[0..key_len]).into_owned(),
+        String::from_utf8_lossy(&buf[key_len..(len)]).into_owned(),
+    ))
+}
+
+fn read_from_file(file: &mut File, position: &Position) -> Result<(String, String)> {
+    file.seek(SeekFrom::Start(position.offset + 3))?;
+
+    let key_len = position.key_len as usize;
+    let value_len = position.value_len as usize;
     let len = key_len + value_len;
 
     let mut buf = vec![0u8; len as usize];
--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,24 @@ fn main() {
 
     indexed_log.read_all();
 
-    let (rand_key, rand_value) = random_key_value();
-    indexed_log.append(&rand_key, &rand_value);
+    let (rand_key1, rand_value) = random_key_value();
+    indexed_log.append(&rand_key1, &rand_value);
 
-    let (rand_key, rand_value) = random_key_value();
-    indexed_log.append(&rand_key, &rand_value);
+    let (rand_key2, rand_value) = random_key_value();
+    indexed_log.append(&rand_key2, &rand_value);
 
-    let (rand_key, rand_value) = random_key_value();
-    indexed_log.append(&rand_key, &rand_value);
+    let (rand_key3, rand_value) = random_key_value();
+    indexed_log.append(&rand_key3, &rand_value);
 
     indexed_log.print_all();
     indexed_log.read_all();
+
+    println!("Read by key");
+    println!("-----------");
+    println!("{} => {}", rand_key1, indexed_log.read(&rand_key1));
+    println!("{} => {}", rand_key2, indexed_log.read(&rand_key2));
+    println!("{} => {}", rand_key3, indexed_log.read(&rand_key3));
+    println!("-----------");
 }
 
 fn random_key_value() -> (String, String) {
